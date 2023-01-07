17ab2db6fc6e32e535fd4d58cc0de1c51afce436
Karim Shehadeh <kshehadeh@underarmour.com>
2023-01-07 09:00:15 -0500

    Added controller directory with skeletons

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,13 @@
+
+
+/**
+ * The base controller should be used as the parent class for any of the controller
+ * classes.  Controllers are meant to be used as the repository for any business logic
+ * that has to be done.  It acts as a intermediary between the API request layer and the 
+ * data store - doing transformations, applying rules, aggregating data, etc.
+ */
+abstract class Controller {
+
+}
+
+export default Controller
