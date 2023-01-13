6e3475a1d61740b113b35d9c63da8c25da4bb458
Ian Shehadeh <IanShehadeh2020@gmail.com>
2023-01-13 07:28:30 -0500

    document posting

--- backend/src/entity/Posting.ts
+++ backend/src/entity/Posting.ts
@@ -1,6 +1,14 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
 import { Account } from './Account';
 
+
+/**
+ * A Posting is a modification to an accounts' balance.
+ * 
+ * Postings are one side of a transaction.
+ * If one posting moves $10 out of an account there should be one (or more) postings that moves $10 into an account.
+ * i.e. The sum of all postings should always be 0.
+ */
 @Entity()
 export class Posting {
   @PrimaryGeneratedColumn()
