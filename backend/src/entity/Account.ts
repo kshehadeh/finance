b344d815ab93098bbc5156529592dbf364d07f2a
Ian Shehadeh <IanShehadeh2020@gmail.com>
2023-01-12 19:24:35 -0500

    document account model

--- backend/src/entity/Account.ts
+++ backend/src/entity/Account.ts
@@ -1,5 +1,26 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn, Tree } from 'typeorm';
 
+/**
+ * Accounts represent a beginning balance (currently zero, for simplicity's sake) and a series of increases or decreases associated with that balance (see: [[Posting]]).
+ * The balance of an account is the sum of these modifications.
+ *
+ * An account is not necessarily tied to a user's account with an institution, such as bank account or a venmo account.
+ * Although, tracking that balance is one application of this model.
+ * Another application is tracking income or expenses.
+ * For example, the amount of money paid by a client could be represented as an account.
+ *
+ * Accounts are primarily organized as a tree.
+ * Parent accounts' balances include that of their children, i.e. the series of modifications applied to one account also applies to it's parents.
+ * An example of an account tree for a freelance sysadmin could be:
+ * ```txt
+ * income
+ *    sysadmin
+ *       Client 1
+ *       Client 2
+ *       Client 3
+ * ```
+ * Because parent's sum their children's balances, the user could see the sum of their income from system administration with the balance of the sysadmin account.
+ */
 @Entity()
 @Tree('nested-set')
 export class Account {
