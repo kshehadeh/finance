c0ec54efb39ca1807e0182c3693efcd6e539769c
Ian Shehadeh <IanShehadeh2020@gmail.com>
2023-01-20 17:08:13 -0500

    brief documentation for clear date, allow null

--- backend/src/sources/index.ts
+++ backend/src/sources/index.ts
@@ -7,7 +7,10 @@ export abstract class ExternalPosting {
   abstract getAmount(): string; /* using string type for decimal number for now */
 
   abstract getDatePosted(): Date;
-  abstract getDateCleared(): Date;
+
+  /** The date the transaction was cleared with the institution, null means the posting is still pending.
+   */
+  abstract getDateCleared(): Date | null;
 
   abstract getDescription(): string;
 }
